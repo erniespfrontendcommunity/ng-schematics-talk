794c354351b7034f7437f37b945f5d8982e84761
ERNI\cagi <gilbe.cao@gmail.com>
2019-10-23 17:24:00 +0200

    add schematic structure

---
+++ my-collection/src/my-collection/files/__name@dasherize__.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-<%= dasherize(name)%>',
+    template: `
+<mat-card class="dashboard-card">
+    <mat-card-header>
+        <mat-card-title>
+            My awesome card!
+        </mat-card-title>
+    </mat-card-header>
+</mat-card>
+    `
+})
+export class <%= classify(name) %>Component {
+}
--- my-collection/src/my-collection/index.ts
+++ my-collection/src/my-collection/index.ts
@@ -1,10 +1,47 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import {
+  apply,
+  MergeStrategy,
+  mergeWith,
+  move,
+  Rule,
+  SchematicContext,
+  template,
+  Tree,
+  url
+} from '@angular-devkit/schematics';
+import { normalize } from 'path';
+import { getWorkspace } from '@schematics/angular/utility/config';
+import { parseName } from '@schematics/angular/utility/parse-name';
+import { buildDefaultPath } from '@schematics/angular/utility/project';
+import { strings } from '@angular-devkit/core';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function myCollection(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+    setupOptions(tree, _options);
+    const movePath = normalize(_options.path + '/' + strings.dasherize(_options.name));
+    const templateSource = apply(url('./files'), [
+      template({
+        ...strings,
+        ..._options,
+      }),
+      move(movePath),
+    ]);
+
+    const rule = mergeWith(templateSource, MergeStrategy.Default);
+    return rule(tree, _context);
   };
 }
+
+export function setupOptions(host: Tree, _options: any): Tree {
+  const workspace = getWorkspace(host);
+  const projectKey = Object.keys(workspace.projects)[0];
+  const project = workspace.projects[projectKey];
+  _options.path = buildDefaultPath(project);
+  const parsedPath = parseName(_options.path, _options.name);
+  _options.name = parsedPath.name + '-card';
+  return host;
+}
+
